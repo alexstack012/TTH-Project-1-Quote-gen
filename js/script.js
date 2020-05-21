0417322d1e19fec083875ec63ad70ebf6cc581c2
alexstack012 <62245175+alexstack012@users.noreply.github.com>
2020-05-21 05:40:34 -0500

    updated js and HTML files
    updated the HTML file, got rid of the random color generator due to numerous bugs and implemented set colors and made it random with these colors. redid printQuote function.

--- js/script.js
+++ js/script.js
@@ -11,11 +11,7 @@ project 1 - A Random Quote Generator
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-var message = '';
-var red;
-var green;
-var blue;
-var viewedQuotes =[];
+
 
 var quotes = [
 	{
@@ -66,65 +62,90 @@ var quotes = [
  * ` from a website, tags for what catagory they may fall into and the year if known.
 ***/
 
+var colors = [
+
+	{
+        background: "#6639a6", //Purple
+        button: "#521262"
+	},
+    {
+        background: "#3f72af", //Blue
+        button: "#112d4e"
+	},
+	{
+        background: "#3fc1c9", //Aqua
+        button: "#fc5185"
+	},
+	{
+        background: "#00adb5", //Teal
+        button: "#393e46"
+	},
+	{
+        background: "#118df0", //Light Blue
+        button: "#0e2f56"
+    },
+	
+];
+
+let timer;
+
+//I set colors because with random colors sometimes the quotes can be hard to read
 
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
-	var randomQuote = Math.floor(Math.random() * quotes.length);
-	
-	var splicedQuote = quotes.splice(randomQuote, 1)[0];
-	viewedQuotes.push(splicedQuote);
-	if ( quotes.length === 0 ) {
-		quotes = viewedQuotes;
-		viewedQuotes = [];
-	}
-	return splicedQuote;
+    let randomQuote = Math.floor(Math.random() * quotes.length);
+
+    return quotes[randomQuote];
 }
 
+function getRandomColor() {
+    let randomColor = Math.floor(Math.random() * colors.length);
 
-//Random color generator
-
-function randomColorGenerator() {
-	var randomColor;
-	red = Math.floor(Math.random() * 256 );
-	green = Math.floor(Math.random() * 256 );
-	blue = Math.floor(Math.random() * 256 );
-	randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-	return randomColor;
+    return colors[randomColor];
 }
 
-
+// runs the printQuote function after 15 seconds
+function startTimer() {
+    timer = setInterval(printQuote, 15000);
+}
 /***
  * `printQuote` function
 ***/
 
 function printQuote() {
-	// On click event, function printQuote runs, then fires the getRandomQuote function
-	var quotes = getRandomQuote();
-	message ='<p class="quote">' + quotes.quote + '</p>';
-	message  += '<p class="source">' + quotes.source;
-	if ( quotes.citation ) {
-		message += '<span class="citation">' + quotes.citation + '</span>';
-	} else {
-		message += '';
-	}
-	if (quotes.year) {
-		message += '<span class="year">' + quotes.year + '</span></p>';	
-	} else {
-		message += '';
-	}
-	if ( quotes.tags ) {
-		message += '<h3>' + quotes.tags + '</h3>';
-	} else {
-		message += '';
-	}
-	print(message);
-	//Generate random color
-	randomColorGenerator();
-	//Update background with new random color
-	document.getElementById('bgcolor').style.backgroundColor = randomColorGenerator();
+    // creates the currentQuoute variable and sets the value to the random object that is returned when the getRandomQuote function is called
+    // creates the currentColor variable and sets the value to the random object that is returned when the getRandomColor function is called
+    // creates the html variable and uses the currentQuote variable along with key values to build a string
+    let currentQuote = getRandomQuote();
+    let currentColor = getRandomColor();
+    let html = "<p class='quote'> " + currentQuote.quote + "</p>";
+    html += "<p class='source'> " + currentQuote.source;
+    // tests to see if the citation property is present in the currentQuote and if so, adds it to the string
+    if ("citation" in currentQuote) {
+        html += "<span class='citation'> " + currentQuote.citation + "</span>";
+    }
+    // tests to see if the date property is present in the currentQuote and if so, adds it to the string
+    if ("date" in currentQuote) {
+        html += "<span class='year'> " + currentQuote.date + "</span>";
+    }
+    html += "<span class='category'> " + currentQuote.category + "</span>";
+
+    // writes the info from the html variable to the div with the quote-box id
+    // uses the currentColor variable to change the background color
+    // uses the currentColor variable to change the button color
+    document.getElementById("quote-box").innerHTML = html;
+    document.body.style.background = currentColor.background;
+    document.getElementById("loadQuote").style.background = currentColor.button;
+
+    // clears any previous timers that might be running and starts a new one
+    clearTimer();
+    startTimer();
 }
+
+printQuote();
+
 
 /***
  * click event listener for the print quote button
