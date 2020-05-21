4c55aa0d565686775d5705586f93f71bc4e99a11
alexstack012 <62245175+alexstack012@users.noreply.github.com>
2020-05-21 05:45:01 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -115,28 +115,33 @@ function startTimer() {
 ***/
 
 function printQuote() {
-    // creates the currentQuoute variable and sets the value to the random object that is returned when the getRandomQuote function is called
-    // creates the currentColor variable and sets the value to the random object that is returned when the getRandomColor function is called
-    // creates the html variable and uses the currentQuote variable along with key values to build a string
-    let currentQuote = getRandomQuote();
-    let currentColor = getRandomColor();
-    let html = "<p class='quote'> " + currentQuote.quote + "</p>";
-    html += "<p class='source'> " + currentQuote.source;
-    // tests to see if the citation property is present in the currentQuote and if so, adds it to the string
+
+	let currentQuote = getRandomQuote();
+	
+	let currentColor = getRandomColor();
+	
+	let html = "<p class='quote'> " + currentQuote.quote + "</p>";
+	
+	html += "<p class='source'> " + currentQuote.source;
+	
+    // If the citation property is there in the currentQuote and adds it to the string
     if ("citation" in currentQuote) {
         html += "<span class='citation'> " + currentQuote.citation + "</span>";
-    }
-    // tests to see if the date property is present in the currentQuote and if so, adds it to the string
+	}
+	
+    // If the date property is there in the currentQuote and if so, adds it to the string
     if ("date" in currentQuote) {
         html += "<span class='year'> " + currentQuote.date + "</span>";
-    }
+	}
+	
     html += "<span class='category'> " + currentQuote.category + "</span>";
 
-    // writes the info from the html variable to the div with the quote-box id
-    // uses the currentColor variable to change the background color
-    // uses the currentColor variable to change the button color
-    document.getElementById("quote-box").innerHTML = html;
-    document.body.style.background = currentColor.background;
+
+    // currentColor to change the button color and currentColor to change the background color
+	document.getElementById("quote-box").innerHTML = html;
+	
+	document.body.style.background = currentColor.background;
+	
     document.getElementById("loadQuote").style.background = currentColor.button;
 
     // clears any previous timers that might be running and starts a new one
